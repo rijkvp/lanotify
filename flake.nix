c3cabae88eba5b3ca84fc2160359c95c96b981ff
Rijk van Putten <rijk@rijkvp.nl>
2025-05-26 20:28:38 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          buildInputs = [
+            pkgs.arp-scan
+          ];
+        };
+
+        crate = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
+      in
+      {
+        checks = {
+          inherit crate;
+        };
+        packages.default = crate;
+        apps.default = flake-utils.lib.mkApp {
+          drv = crate;
+        };
+      }
+    );
+}
