be163be847c0d0ccec0aea12f8205e8b8d76b396
Rijk van Putten <rijk@rijkvp.nl>
2025-11-01 18:32:02 +0100

    Update deps

--- flake.nix
+++ flake.nix
@@ -3,19 +3,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
       nixpkgs,
       crane,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.mkLib pkgs;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
 
         commonArgs = {
           src = craneLib.cleanCargoSource ./.;
@@ -43,6 +51,13 @@
         apps.default = flake-utils.lib.mkApp {
           drv = crate;
         };
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
+            arp-scan
+            cargo-edit
+          ];
+        };
+
       }
     );
 }
